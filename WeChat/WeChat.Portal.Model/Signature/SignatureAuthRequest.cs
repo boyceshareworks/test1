a6daeb52436e48d2bfd1f38222ec20ccce5e36fc
boyceshareworks <boyce.yang@shareworks.cn>
2016-09-30 16:54:26 +0800

    add framework

---
+++ WeChat/WeChat.Portal.Model/Signature/SignatureAuthRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WeChat.Portal.Model.Signature
+{
+    public class SignatureAuthRequest
+    {
+        public string Signature { get; set; }
+        public string TimeStamp { get; set; }
+        public string Nonce { get; set; }
+        public string EchoStr { get; set; }
+    }
+}
